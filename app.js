8a44c448122a28b5841744c65b14bb0ec8369c1a
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-07-31 16:39:31 +0000

    Added the routes and template files for all pages

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
 const app = express();
+import indexRouter from "./routes/index.js";
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(express.static("public"));
+app.use("/", indexRouter);
 
 app.listen(8000, () => console.log(`Server is running on port 8000 `));
