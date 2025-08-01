ab508f04edc33677e81af21f684134b316b57828
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-08-01 13:50:21 +0000

    Fixing some deployment issues

--- app.js
+++ app.js
@@ -8,5 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/", indexRouter);
 
-export default app ;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
